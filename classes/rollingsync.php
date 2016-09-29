c3c98038a64fa2afb4a9b93f74f09abfc591ba66
Matt Davidson <davidso1@rose-hulman.edu>
2016-09-29 10:59:15 -0400

    62_patch

--- classes/rollingsync.php
+++ classes/rollingsync.php
@@ -32,6 +32,14 @@ require_once(dirname(__FILE__) . '/../lib/panopto_data.php');
  * @copyright Panopto 2009 - 2015
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  *
+ * Upon course creation: coursecreated is triggered
+ * Upon enroll of user: enrollmentcreated AND roleadded is triggered
+ * Upon unassigning role: roledeleted is triggered
+ * Upon reassigning role: roleadded is triggered
+ * Upon setting enrollment status to suspended: enrolmentupdated is triggered
+ * Upon setting enrollment status to reactivated: enrolmentupdated is triggered
+ * Upon unenroll of user: roledeleted AND enrollmentdeleted is triggered
+ * 
  */
 class block_panopto_rollingsync {
 
@@ -82,6 +90,44 @@ class block_panopto_rollingsync {
             'contextid' => $event->contextid,
             'eventtype' => "enroll_remove"
         ));
+
+        if ($CFG->block_panopto_async_tasks) {
+            \core\task\manager::queue_adhoc_task($task);
+        } else {
+            $task->execute();
+        }
+    }
+
+    /**
+     * Called when an enrolment has been suspended or reactivated
+     * but not when new enrollments are added or when enrollments are removed.
+     */
+    public static function enrolmentupdated(\core\event\user_enrolment_updated $event) {
+        global $CFG, $DB;
+        
+        if (\panopto_data::get_panopto_course_id($event->courseid) === false
+            || $CFG->version < self::$requiredVersion) {
+            return;
+        }
+        
+        $task = new \block_panopto\task\update_user();
+
+        $context = context_course::instance($event->courseid);
+        if(is_enrolled($context, $event->relateduserid, '', true)) { // User is enrolled.  Make sure they are added in Panopto
+            $task->set_custom_data(array(
+                'courseid' => $event->courseid,
+                'relateduserid' => $event->relateduserid,
+                'contextid' => $event->contextid,
+                'eventtype' => "enroll_add"
+            ));
+        } else { // User is unenrolled or suspended.  Make sure they are removed from Panopto
+            $task->set_custom_data(array(
+                'courseid' => $event->courseid,
+                'relateduserid' => $event->relateduserid,
+                'contextid' => $event->contextid,
+                'eventtype' => "enroll_remove"
+            ));
+        }
 
         if ($CFG->block_panopto_async_tasks) {
             \core\task\manager::queue_adhoc_task($task);
